6e3ff36dab3dea6559b0add577707ff9b1dc79e6
Andreas Marinis <116451022+andrew6597@users.noreply.github.com>
2024-08-12 11:19:09 +0300

    Update model_builider_tf.py

--- TensorFLow_Version/model_builider_tf.py
+++ TensorFLow_Version/model_builider_tf.py
@@ -28,9 +28,8 @@ def TransLOB(window_size,n_dim):
     i = Input(shape=(window_size, n_dim))
     x = i
     # 5 Dilated Convolution Layers
-    x = tf.keras.layers.Conv1D(64, kernel_size=2, dilation_rate=1, activation='relu', padding='causal')(x)
-    x = tf.keras.layers.Conv1D(32, kernel_size=2, dilation_rate=4, activation='relu', padding='causal')(x)
-    x = tf.keras.layers.Conv1D(14, kernel_size=2, dilation_rate=16, activation='relu', padding='causal')(x)
+    x = tf.keras.layers.Conv1D(32, kernel_size=2, dilation_rate=2, activation='relu', padding='causal')(x)
+    x = tf.keras.layers.Conv1D(14, kernel_size=2, dilation_rate=8, activation='relu', padding='causal')(x)
 
     # Layer Norm
     x = tf.keras.layers.LayerNormalization()(x)
@@ -47,7 +46,7 @@ def TransLOB(window_size,n_dim):
 
     # MLP
     x = tf.keras.layers.Flatten()(x)
-    x = Dense(64, activation='relu', kernel_regularizer='l2', kernel_initializer='glorot_uniform')(x)
+    x = Dense(16, activation='relu', kernel_regularizer='l2', kernel_initializer='glorot_uniform')(x)
 
     # Dropout
     x = Dropout(0.1)(x)
